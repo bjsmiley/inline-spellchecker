903799c8a617e0119d09807454eaa745ebe056f2
Bryan Smiley <bjsmiley@ucdavis.edu>
2023-03-05 00:13:22 -0500

    switch to native spell checker

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,58 @@
+use windows::{Win32::{Globalization::{ISpellCheckerFactory, SpellCheckerFactory, CORRECTIVE_ACTION_DELETE, CORRECTIVE_ACTION_REPLACE, CORRECTIVE_ACTION_GET_SUGGESTIONS, CORRECTIVE_ACTION_NONE, }, System::Com::{CoInitializeEx, CoCreateInstance, CLSCTX_ALL, COINIT_MULTITHREADED}}, core::{HSTRING, PWSTR}, w};
 
-fn main() -> Result<(),srv::Error>{
+fn main() -> windows::core::Result<()>{
     
     let utterance = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+    unsafe { CoInitializeEx(None, COINIT_MULTITHREADED)?; }
+    let factory: ISpellCheckerFactory = unsafe { CoCreateInstance(&SpellCheckerFactory, None, CLSCTX_ALL)? };
+    let checker = unsafe { factory.CreateSpellChecker(w!("en-US"))? };
+    let result = unsafe { checker.ComprehensiveCheck(&HSTRING::from(utterance.clone()))? };
+    let input = utterance.clone().as_str();
+    let output: &mut Vec<String> = &mut Vec::new();
+    let mut current_index: usize = 0;
 
-    let corrected = srv::spellcheck(utterance)?;
+    while let Ok(err) = unsafe { result.Next() } {
+        let index = usize::try_from(unsafe { err.StartIndex()? }).unwrap();
+        let len = usize::try_from(unsafe { err.Length()? }).unwrap();
+        let correction = unsafe { err.CorrectiveAction()? };
 
-    println!("{:?}", corrected);
+        let before = &utterance[current_index..index];
+        output.push(before.to_string());
+
+        match correction {
+            CORRECTIVE_ACTION_DELETE => {
+            },
+            CORRECTIVE_ACTION_REPLACE => {
+                let replacement = unsafe { err.Replacement()?.to_string()? };                
+                output.push(replacement);
+            },
+            CORRECTIVE_ACTION_GET_SUGGESTIONS => {
+                let word = &utterance[index..index + len];
+                let suggestions = unsafe { checker.Suggest(&HSTRING::from(word))? };
+                let mut suggestion = [PWSTR::null()];
+                unsafe { let _ = suggestions.Next(&mut suggestion, None); }
+                if suggestion[0].is_null() {
+                    output.push(word.to_string());
+                }
+                else {
+                    let replacement = unsafe { err.Replacement()?.to_string()? };                
+                    output.push(replacement);
+                }
+            },
+            CORRECTIVE_ACTION_NONE => {
+                let word = &utterance[index..index + len];
+                output.push(word.to_string());
+            },
+            _ => {}
+        }
+        current_index = index + len;
+    }
+
+    println!("{}",output.join(""));
 
     Ok(())
 }
 
-mod core {
-
-    fn exec() {
-
-    }
-}
 
 
-mod srv {
-    use reqwest::{blocking::{ClientBuilder}, Method};
 
-
-    const BING_URI: &str = "https://api.bing.microsoft.com/v7.0/spellcheck";
-    const BING_KEY: &str = "";
-    const MARKET_PARAM: &str = "en-US";
-    const MODE_PARAM: &str = "spell";
-
-    #[derive(Debug, serde::Deserialize)]
-    pub struct BingResponse {
-        pub errors: Option<String>,
-        pub flagged_tokens: Vec<Token>
-    }
-
-    #[derive(Debug, serde::Deserialize)]
-    pub struct Token {
-        pub offset: usize,
-        pub suggestions: Vec<Suggestion>
-    }
-
-    #[derive(Debug, serde::Deserialize)]
-    pub struct Suggestion {
-        pub suggestion: String,
-        pub score: u32
-    }
-
-    pub fn spellcheck(text: String) -> Result<String,Error> {
-
-        let client = ClientBuilder::new().build().map_err(|_| Error::Init)?;
-
-        let result = client.request(Method::POST, BING_URI)
-            .query(&[("text", text), ("mkt", MARKET_PARAM.to_owned()), ("mode", MODE_PARAM.to_owned())])
-            .header("Ocp-Apim-Subscription-Key", BING_KEY)
-            .header("content-length", 0)
-            .send();
-
-        let res: BingResponse = result.map_err(|e| {
-            if e.is_status() {
-                return Error::Status(e.status().unwrap().as_u16())
-            }
-            Error::Other(e)
-        })?.json().unwrap();
-
-        if let Some(e) =  res.errors {
-            return Err(Error::Bing(e));
-        }
-        if res.flagged_tokens.len() == 0 {
-            return Ok(text)
-        }
-        let words: Vec<&str> = text.split_whitespace().collect();
-        for w in res.flagged_tokens.into_iter().filter(|x| x.suggestions.len() > 0) {
-            words[w.offset] = w.suggestions[0].suggestion.clone();
-        }
-
-        Ok(words.join(" "))
-    }
-
-    #[derive(Debug)]
-    pub enum Error {
-        Init, // a TLS backend cannot be initialized, or the resolver cannot load the system configuration.
-        Status(u16),
-        Bing(String),
-        Other(reqwest::Error)
-    }
-}
